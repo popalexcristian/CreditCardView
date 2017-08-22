f6f9791f5defed2dae82576805ff75f9e7b98f96
Alex Cristian <alex.cristian.pop@tapptitude.com>
2017-08-22 10:50:01 +0300

    Remove views init

--- app/src/main/java/com/cooltechworks/creditcarddesign/sample/MainActivity.java
+++ app/src/main/java/com/cooltechworks/creditcarddesign/sample/MainActivity.java
@@ -1,16 +1,16 @@
 package com.cooltechworks.creditcarddesign.sample;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.cooltechworks.checkoutflow.R;
-import com.cooltechworks.creditcarddesign.CreditCardView;
 import com.cooltechworks.creditcarddesign.CardEditActivity;
 import com.cooltechworks.creditcarddesign.CreditCardUtils;
+import com.cooltechworks.creditcarddesign.CreditCardView;
 
 /**
  * Created by glarencezhao on 10/23/16.
@@ -127,7 +127,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }
-
     }
-
 }
