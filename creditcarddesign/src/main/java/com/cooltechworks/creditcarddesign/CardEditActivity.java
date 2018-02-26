713707f695f86f96daae414037237a6db9f1929e
Alex Pop <alex.cristian.pop@tapptitude.com>
2018-02-26 12:19:41 +0200

    Remove card scanner and update libraries

--- creditcarddesign/src/main/java/com/cooltechworks/creditcarddesign/CardEditActivity.java
+++ creditcarddesign/src/main/java/com/cooltechworks/creditcarddesign/CardEditActivity.java
@@ -3,15 +3,12 @@ package com.cooltechworks.creditcarddesign;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.stripe.android.view.CardInputWidget;
@@ -19,29 +16,23 @@ import com.stripe.android.view.CardNumberEditText;
 import com.stripe.android.view.ExpiryDateEditText;
 import com.stripe.android.view.StripeEditText;
 
-import io.card.payment.CardIOActivity;
-import io.card.payment.CreditCard;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
-import static android.support.design.widget.Snackbar.LENGTH_SHORT;
 import static android.view.inputmethod.EditorInfo.IME_ACTION_DONE;
 import static com.cooltechworks.creditcarddesign.CreditCardUtils.EXTRA_CARD_BRAND;
 import static com.cooltechworks.creditcarddesign.CreditCardUtils.EXTRA_CARD_CVV;
 import static com.cooltechworks.creditcarddesign.CreditCardUtils.EXTRA_CARD_EXPIRY;
 import static com.cooltechworks.creditcarddesign.CreditCardUtils.EXTRA_CARD_NUMBER;
-import static io.card.payment.CardIOActivity.EXTRA_SCAN_RESULT;
 
 
 public class CardEditActivity extends AppCompatActivity implements CardInputWidget.CardInputListener {
 
-    private static final int MY_SCAN_REQUEST_CODE = 1;
     private CreditCardView mCreditCardView;
     private CardInputWidget mCardInputWidget;
     private CardNumberEditText mCardNumberEt;
     private ExpiryDateEditText mExpiryDateEt;
     private StripeEditText mCvcNumberEt;
 
-    private ImageView mCardScan;
     private Toolbar mToolbar;
 
     @Override
@@ -53,7 +44,6 @@ public class CardEditActivity extends AppCompatActivity implements CardInputWidg
         mCreditCardView = (CreditCardView) findViewById(com.cooltechworks.creditcarddesign.R.id.credit_card_view);
         mCardInputWidget = (CardInputWidget) findViewById(R.id.card_input_widget);
         mCardInputWidget.setCardInputListener(this);
-        mCardScan = (ImageView) findViewById(R.id.card_scan_iv);
 
         mCardNumberEt = (CardNumberEditText) mCardInputWidget.findViewById(com.stripe.android.R.id.et_card_number);
 
@@ -162,53 +152,6 @@ public class CardEditActivity extends AppCompatActivity implements CardInputWidg
                 return false;
             }
         });
-
-        mCardScan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent scanIntent = new Intent(CardEditActivity.this, CardIOActivity.class);
-                scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_EXPIRY, true); // default: false
-                scanIntent.putExtra(CardIOActivity.EXTRA_SCAN_EXPIRY, true);
-                scanIntent.putExtra(CardIOActivity.EXTRA_REQUIRE_CVV, true);
-
-                startActivityForResult(scanIntent, MY_SCAN_REQUEST_CODE);
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == MY_SCAN_REQUEST_CODE) {
-            if (data != null && data.hasExtra(EXTRA_SCAN_RESULT)) {
-                CreditCard scanResult = data.getParcelableExtra(EXTRA_SCAN_RESULT);
-
-                mCreditCardView.setCardNumber(scanResult.getFormattedCardNumber());
-                mCardNumberEt.setText(scanResult.getFormattedCardNumber());
-
-                if (scanResult.isExpiryValid()) {
-                    int expirationYear = scanResult.expiryYear % 100;
-                    if (scanResult.expiryMonth < 10) {
-                        String expiryMonth = String.format("%02d", scanResult.expiryMonth);
-
-                        mCreditCardView.setCardExpiry(String.format(getString(R.string.expiration_date_placeholder), expiryMonth, expirationYear));
-                        mExpiryDateEt.setText(String.format(getString(R.string.expiration_date_placeholder), expiryMonth, expirationYear));
-                    } else {
-                        mCreditCardView.setCardExpiry(String.format(getString(R.string.expiration_date_placeholder), String.valueOf(scanResult.expiryMonth), expirationYear));
-                        mExpiryDateEt.setText(String.format(getString(R.string.expiration_date_placeholder), String.valueOf(scanResult.expiryMonth), expirationYear));
-                    }
-                    onCardComplete();
-                    onExpirationComplete();
-                }
-
-                if (scanResult.cvv != null) {
-                    mCreditCardView.setCVV(scanResult.cvv);
-                    mCvcNumberEt.setText(scanResult.cvv);
-                }
-
-            } else {
-                Snackbar.make(findViewById(android.R.id.content), "Scan was cancelled", LENGTH_SHORT).show();
-            }
-        }
     }
 
     @Override
